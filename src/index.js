335bb5f59f8eec42c0ddb9c1d3ed05e46e37c58f
Marc Roma <marc.roma@cirvianum.cat>
2024-02-21 19:05:51 +0100

    Sync

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // REQUIRE STATEMENTS ==============================================
+require("dotenv").config();
 const axios = require("axios");
 const { Client } = require("@notionhq/client");
+const { exit } = require("process");
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
@@ -8,15 +10,50 @@ const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 // FUNCTION DEFINITIONS ============================================
 
-// Get Data from database from Notion
-const getData = async () => {
+// Universal Function to Get Data from X ---------------------------
+
+const getDataFromX = async (databaseId) => {
   (async () => {
-    const databaseId = NOTION_DATABASE_ID;
     const response = await notion.databases.query({
       database_id: databaseId,
     });
-    console.log(response);
+
+    return response.results;
   })().catch(console.error);
 };
 
-getData();
+// Get Data from Home Page from Notion -----------------------------
+const getDataFromHome = async () => {
+  const response = getDataFromX(process.env.NOTION_HOME_ID);
+
+  console.log(response.results);
+};
+
+// getDataFromHome();
+
+// -----------------------------------------------------------------
+
+// Get Alll Projects from Notion ----------------------------
+
+const getAllProjects = async () => {
+  (async () => {
+    const response = getDataFromX(process.env.NOTION_PROJECTS_ID);
+
+    // console.log(response.results);
+    return response.results;
+  })().catch(console.error);
+};
+
+getAllProjects();
+
+// -----------------------------------------------------------------
+
+// Get All Project Names from Notion -------------------------------
+
+const getProjectNames = async () => {
+ 
+};
+
+getProjectNames();
+
+// =================================================================
