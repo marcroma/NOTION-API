9673373cf7d08db5f58d572d0ef42fc7bb1bd55f
Marc Roma <marc.roma@cirvianum.cat>
2024-02-22 09:24:39 +0100

    GetProjectNames OK

--- src/index.js
+++ src/index.js
@@ -13,45 +13,67 @@ const notion = new Client({ auth: process.env.NOTION_TOKEN });
 // Universal Function to Get Data from X ---------------------------
 
 const getDataFromX = async (databaseId) => {
-  (async () => {
+  try {
     const response = await notion.databases.query({
       database_id: databaseId,
     });
 
+    // console.log(response.results);
     return response.results;
-  })().catch(console.error);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Get Data from Home Page from Notion -----------------------------
 const getDataFromHome = async () => {
-  const response = getDataFromX(process.env.NOTION_HOME_ID);
+  try {
+    const response = await getDataFromX(process.env.NOTION_HOME_ID);
 
-  console.log(response.results);
+    // console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-// getDataFromHome();
+getDataFromHome();
 
 // -----------------------------------------------------------------
 
 // Get Alll Projects from Notion ----------------------------
 
 const getAllProjects = async () => {
-  (async () => {
-    const response = getDataFromX(process.env.NOTION_PROJECTS_ID);
+  try {
+    const response = await getDataFromX(process.env.NOTION_PROJECTS_ID);
 
-    // console.log(response.results);
-    return response.results;
-  })().catch(console.error);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-getAllProjects();
+// getAllProjects();
 
 // -----------------------------------------------------------------
 
 // Get All Project Names from Notion -------------------------------
 
 const getProjectNames = async () => {
- 
+  try {
+    const response = await getDataFromX(process.env.NOTION_PROJECTS_ID);
+
+    const projectNames = response.map((project) => {
+      return project.properties.Name.title[0].plain_text;
+    });
+
+    console.log(projectNames);
+    return projectNames;
+    
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 getProjectNames();
